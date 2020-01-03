cbf7d8c61301242626c374f8f1e81b51280f7701
Chris <cjr6439@gmail.com>
2020-01-02 21:10:16 -0600

    1.4 modified to use array of objects in variable

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -3,29 +3,31 @@ import ReactDOM from "react-dom";
 
 const App = () => {
     const course = "Half Stack application development";
-    const part1 = {
-        name: "Fundamentals of React",
-        exercises: 10
-    };
-    const part2 = {
-        name: "Using props to pass data",
-        exercises: 7
-    };
-    const part3 = {
-        name: "State of a component",
-        exercises: 14
-    };
+    const parts = [
+        {
+            name: "Fundamentals of React",
+            exercises: 10
+        },
+        {
+            name: "Using props to pass data",
+            exercises: 7
+        },
+        {
+            name: "State of a component",
+            exercises: 14
+        }
+    ];
 
     return (
         <>
             <Header course={course} />
-            <Part name={part1.name} exercises={part1.exercises} />
-            <Part name={part2.name} exercises={part2.exercises} />
-            <Part name={part3.name} exercises={part3.exercises} />
+            <Part name={parts[0].name} exercises={parts[0].exercises} />
+            <Part name={parts[1].name} exercises={parts[1].exercises} />
+            <Part name={parts[2].name} exercises={parts[2].exercises} />
             <Total
-                exercises1={part1.exercises}
-                exercises2={part2.exercises}
-                exercises3={part3.exercises}
+                exercises1={parts[0].exercises}
+                exercises2={parts[1].exercises}
+                exercises3={parts[2].exercises}
             />
         </>
     );
