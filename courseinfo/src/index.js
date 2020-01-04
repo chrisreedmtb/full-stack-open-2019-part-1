f2f519034d739b93d6371da5b452688c29368035
Chris <cjr6439@gmail.com>
2020-01-03 18:25:16 -0600

    1.5 refactored to make course a single JS object

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -2,27 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-    const course = "Half Stack application development";
-    const parts = [
-        {
-            name: "Fundamentals of React",
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
             exercises: 10
-        },
-        {
-            name: "Using props to pass data",
+          },
+          {
+            name: 'Using props to pass data',
             exercises: 7
-        },
-        {
-            name: "State of a component",
+          },
+          {
+            name: 'State of a component',
             exercises: 14
-        }
-    ];
+          }
+        ]
+      }
 
     return (
         <>
             <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Content course={course} />
+            <Total course={course} />
         </>
     );
 };
@@ -30,7 +32,7 @@ const App = () => {
 const Header = props => {
     return (
         <>
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         </>
     );
 };
@@ -38,14 +40,15 @@ const Header = props => {
 const Content = props => {
     return (
       <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />       
+        <Part part={props.course.parts[0]} />
+        <Part part={props.course.parts[1]} />
+        <Part part={props.course.parts[2]} />       
       </div>
     )
   }
 
 const Part = props => {  
+    console.log(props)
     return (
         <>
             <p>
@@ -60,7 +63,7 @@ const Total = props => {
         <>
             <p>
                 Number of exercises{" "}
-                {props.parts[0].exercises + props.parts[1].exercises  + props.parts[2].exercises}
+                {props.course.parts[0].exercises + props.course.parts[1].exercises  + props.course.parts[2].exercises}
             </p>
         </>
     );
